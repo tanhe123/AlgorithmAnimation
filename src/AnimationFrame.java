c569250a63d012f69c5a36cdff1935322596ff96
tanhehe <443016215@qq.com>
2013-10-27 21:06:33 +0800

    fix

--- src/AnimationFrame.java
+++ src/AnimationFrame.java
@@ -1,21 +1,41 @@
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class AnimationFrame extends JFrame{
 	public AnimationFrame() {
 		ArrayComponent comp = new ArrayComponent();
 		add(comp, BorderLayout.CENTER);
+					
+		final Sorter sorter = new Sorter(comp);
 		
-		Double[] values = new Double[20];
-		for (int i=0; i<values.length; i++) {
-			values[i] = new Double(Math.random());
-			comp.setValues(values, values[0], values[1]);
-		}
-			
+		JButton runButton = new JButton("Run");
+		runButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sorter.setRun();
+			}
+		});
+		
+		JButton stepButton = new JButton("Step");
+		stepButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sorter.setStep();
+			}
+		});
+		
+		JPanel buttons = new JPanel();
+		buttons.add(runButton);
+		buttons.add(stepButton);
+		add(buttons, BorderLayout.NORTH);
+				
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-		setTitle("AnimationFrame");
+		//setTitle("AnimationFrame");
+		new Thread(sorter).start();
 	}
 	
 	private static final int DEFAULT_WIDTH = 300;
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,4 +1,60 @@
+import java.awt.Component;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.concurrent.Semaphore;
 
-public class Sorter {
 
+public class Sorter implements Runnable{
+	/**
+	 * Construts a Sorter
+	 * @param comp values the array to be sorted
+	 * comp the componet on which to display the sorting progress
+	 */
+	public Sorter(ArrayComponent comp) {
+		values = new Double[VALUES_LENGTH];
+		for (int i=0; i<values.length; i++)
+			values[i] = new Double(Math.random());
+				
+		this.component = comp;
+		this.run = false;
+		this.gate = new Semaphore(1);
+	}
+	
+	public void setRun() {
+		run = true;
+		gate.release();
+	}
+	
+	public void setStep() {
+		run = false;
+		gate.release();
+	}
+	
+	public void run() {
+		Comparator<Double> comp = new Comparator<Double>() {
+			public int compare(Double i1, Double i2) {
+				component.setValues(values, i1, i2);
+				try {
+					if(run) Thread.sleep(DELAY);
+					else gate.acquire();
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+				}
+				
+				return i1.compareTo(i2);
+			}
+		};
+		
+		Arrays.sort(values, comp);
+		component.setValues(values, null, null);
+	}
+	
+	private Double[] values;
+	
+	private ArrayComponent component;
+	private boolean run;
+	private Semaphore gate;
+	
+	private static final int DELAY = 100;
+	private static final int VALUES_LENGTH = 30;
 }
